90c7246151eabf030c7e2542f3fa2e4236a345a2
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-06-25 02:56:53 +0500

    fix: исправил ошибки расположения элементов

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -19,7 +19,7 @@ function MoviesCardList({ moviesCards, buttonType, isMovieSaved, onSaveMovie, pl
   });
 
   return (
-    <section className={`movies-card-list ${place === 'saved-movies' && 'movies-card-list_place_saved-movies'}`}>
+    <section className={`movies-card-list ${place === 'saved-movies' ? 'movies-card-list_place_saved-movies' : ''}`}>
       {!moviesCards || moviesCards.length === 0 ? (
         <p className='movies-card-list__not-found'>Фильмы по запросу не найдены</p>
       ) : (
