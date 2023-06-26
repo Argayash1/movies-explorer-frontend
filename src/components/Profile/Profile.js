e1066760efaf5ef64fe9ac91a16d5fc9f6c3c7c6
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-06-26 11:10:12 +0500

    fix: удалил абсолютное позиционирование в инпуте в компоненте Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -15,36 +15,37 @@ function Profile({ user, isEdit, onSignOut, onSubmit, onEditProfile, isFormValid
           onSubmit={onSubmit}
           isFormValid={isFormValid}
         >
-          <label className='profile__input-label'>
+          <label htmlFor='name' className='profile__input-label'>
             Имя
-            <input
-              className='profile__input'
-              type='text'
-              name='name'
-              id='name'
-              placeholder='Имя'
-              autoComplete='off'
-              minLength='2'
-              maxLength='30'
-              defaultValue={user.name}
-              required
-              disabled={!isEdit && true}
-            />
           </label>
-          <label className='profile__input-label'>
+          <input
+            className='profile__input'
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Имя'
+            autoComplete='off'
+            minLength='2'
+            maxLength='30'
+            defaultValue={user.name}
+            required
+            disabled={!isEdit && true}
+          />
+          <label htmlFor='' className='profile__input-label profile__label_type_e-mail'>
             E-mail
-            <input
-              className='profile__input profile__input_type_e-mail'
-              type='email'
-              name='email'
-              id='email'
-              placeholder='E-mail'
-              autoComplete='off'
-              defaultValue={user.email}
-              required
-              disabled={!isEdit && true}
-            />
           </label>
+          <input
+            className='profile__input profile__input_type_e-mail'
+            type='email'
+            name='email'
+            id='email'
+            placeholder='E-mail'
+            autoComplete='off'
+            defaultValue={user.email}
+            required
+            disabled={!isEdit && true}
+          />
+
           {isEdit && !isFormValid && <span className='profile__error'>При обновлении профиля произошла ошибка.</span>}
         </Form>
         {!isEdit && (
